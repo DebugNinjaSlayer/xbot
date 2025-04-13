d21119cb5a24cbc87dc09d85089ddd4d5a1f1db2
DebugNinjaSlayer <8620373+DebugNinjaSlayer@users.noreply.github.com>
2025-04-13 19:00:47 +0800

    refactor: Optimize code for improved performance and readability

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,9 @@
-import dotenv from "dotenv";
-import express, { Request, Response } from "express";
-import session from "express-session";
 import { Context, Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
-import TwitterApi from "twitter-api-v2";
-import { getAuthUrl, tweetText } from "./x";
-
-declare module "express-session" {
-  interface SessionData {
-    oauth_token_secret?: string;
-  }
-}
-
-dotenv.config();
+import app from "./routes";
+import { tweetText } from "./x";
 
 const bot = new Telegraf<Context>(process.env.BOT_TOKEN as string);
-const app = express();
-app.set("trust proxy", 1);
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET ?? "you should change this",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: true,
-      maxAge: 3600000,
-    },
-  })
-);
 
 bot.on(message("photo"), async (ctx) => {
   let imageId = ctx.message.photo.pop()?.file_id;
@@ -41,50 +17,12 @@ bot.on(message("photo"), async (ctx) => {
 
 bot.on(message("text"), async (ctx) => {
   const msg = ctx.message.text;
-  await ctx.reply(`Hello ${msg}`);
   await tweetText(msg);
+  await ctx.reply(`Tweeted text: ${msg}`);
 });
 
 bot.launch();
 console.log("Bot is running...");
-
-app.get("/hello", async (req: Request, res: Response) => {
-  const authLink = await getAuthUrl(
-    new TwitterApi({
-      appKey: process.env.TWITTER_APP_KEY as string,
-      appSecret: process.env.TWITTER_APP_SECRET as string,
-    })
-  );
-  req.session.oauth_token_secret = authLink.oauth_token_secret;
-  res.redirect(authLink.url);
-});
-
-app.get("/callback", (req: Request, res: Response) => {
-  const { oauth_token, oauth_verifier } = req.query;
-  const { oauth_token_secret } = req.session;
-
-  if (!oauth_token || !oauth_verifier || !oauth_token_secret) {
-    res.status(400).send("You denied the app or your session expired!");
-    return;
-  }
-
-  const tmpClient = new TwitterApi({
-    appKey: process.env.TWITTER_APP_KEY as string,
-    appSecret: process.env.TWITTER_APP_SECRET as string,
-    accessToken: oauth_token as string,
-    accessSecret: oauth_token_secret as string,
-  });
-
-  tmpClient
-    .login(oauth_verifier as string)
-    .then(({ client: loggedClient, accessToken, accessSecret }) => {
-      console.log("accessToken");
-      console.log(accessToken);
-      console.log("accessSecret");
-      console.log(accessSecret);
-    })
-    .catch(() => res.status(403).send("Invalid verifier or access tokens!"));
-});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ src/routes.ts
@@ -0,0 +1,71 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import session from "express-session";
+import TwitterApi from "twitter-api-v2";
+import { getAuthUrl } from "./x";
+
+declare module "express-session" {
+  interface SessionData {
+    oauth_token_secret?: string;
+  }
+}
+
+dotenv.config();
+
+const app = express();
+app.set("trust proxy", 1);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET ?? "you should change this",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: true,
+      maxAge: 3600000,
+    },
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.get("/hello", async (req: Request, res: Response) => {
+  const authLink = await getAuthUrl(
+    new TwitterApi({
+      appKey: process.env.TWITTER_APP_KEY as string,
+      appSecret: process.env.TWITTER_APP_SECRET as string,
+    })
+  );
+  req.session.oauth_token_secret = authLink.oauth_token_secret;
+  res.redirect(authLink.url);
+});
+
+app.get("/callback", (req: Request, res: Response) => {
+  const { oauth_token, oauth_verifier } = req.query;
+  const { oauth_token_secret } = req.session;
+
+  if (!oauth_token || !oauth_verifier || !oauth_token_secret) {
+    res.status(400).send("You denied the app or your session expired!");
+    return;
+  }
+
+  const tmpClient = new TwitterApi({
+    appKey: process.env.TWITTER_APP_KEY as string,
+    appSecret: process.env.TWITTER_APP_SECRET as string,
+    accessToken: oauth_token as string,
+    accessSecret: oauth_token_secret as string,
+  });
+
+  tmpClient
+    .login(oauth_verifier as string)
+    .then(({ client: loggedClient, accessToken, accessSecret }) => {
+      console.log("accessToken");
+      console.log(accessToken);
+      console.log("accessSecret");
+      console.log(accessSecret);
+    })
+    .catch(() => res.status(403).send("Invalid verifier or access tokens!"));
+});
+
+export default app;
