4d48edf1187f2ee3ed24f3226d82d763cb19b3fd
DebugNinjaSlayer <8620373+DebugNinjaSlayer@users.noreply.github.com>
2025-04-17 17:12:12 +0800

    feat: Implement scheduled task to fetch a record from KV and post to X

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import * as lt from "long-timeout";
+import cron from "node-cron";
 import { Context, Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
-import { getRandomKv, putKv } from "./kv";
+import { deleteKv, getRandomKv, putKv } from "./kv";
 import app from "./routes";
-import { tweetText } from "./x";
-
+import { tweetImages, tweetText } from "./x";
 const bot = new Telegraf<Context>(process.env.BOT_TOKEN as string);
 
 bot.on(message("photo"), async (ctx) => {
@@ -53,6 +54,37 @@ console.log("Bot is running...");
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+cron.schedule(
+  process.env.CRON_SCHEDULE ?? "0 8-22/2 * * *",
+  async () => {
+    const delay = Math.floor(Math.random() * 1000 * 60 * 90) + 1;
+    lt.setTimeout(async function () {
+      try {
+        const kv = await getRandomKv();
+        if (!kv) {
+          console.log("No kv found");
+          return;
+        }
+        const { value, key } = kv;
+        const { imageUrl, caption } = JSON.parse(value);
+        await tweetImages([new URL(imageUrl)], caption);
+        await deleteKv(key);
+        console.log(`Tweeted and deleted kv: ${caption}`);
+      } catch (error) {
+        console.error(error);
+      }
+    }, delay);
+    console.log(
+      `Scheduled tweet at ${new Date(Date.now() + delay).toLocaleString()}`
+    );
+  },
+  {
+    name: "scheduled-tweet",
+    scheduled: true,
+    timezone: "Asia/Shanghai",
+  }
+);
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
